bf917a31c5a7ef6a462f142951b95820619f0735
drewrwilson <drew.r.wilson@gmail.com>
2014-03-06 19:55:01 -0800

    add random greeting text

--- cutiesindever.rb
+++ cutiesindever.rb
@@ -15,12 +15,18 @@ response =  Net::HTTP.get_response(URI.parse(url))
 # get json
 # parson json
 
-client = Twitter::REST::Client.new do |config|
-	config.consumer_key = ENV.fetch('consumer_key')
-	config.consumer_secret = ENV.fetch('consumer_secret')
-	config.access_token = ENV.fetch('access_token')
-	config.access_token_secret = ENV.fetch('access_token_secret')
-end
+#client = Twitter::REST::Client.new do |config|
+#	config.consumer_key = ENV.fetch('consumer_key')
+#	config.consumer_secret = ENV.fetch('consumer_secret')
+#	config.access_token = ENV.fetch('access_token')
+#	config.access_token_secret = ENV.fetch('access_token_secret')
+#end
+
+#parse greetings json
+greetings = JSON.parse( IO.read("greetings.json") )
+
+#grab a random greeting from the list
+randomgreeting = greetings[rand(greetings.length)]
 
 #parse the json into a hash
 animaldata = JSON.parse(response.body);
@@ -29,7 +35,7 @@ animaldata = JSON.parse(response.body);
 animaldata['link'] = "http://www.petharbor.com/pet.asp?uaid=DNVR." + animaldata['id']
 
 #building the tweet sentence
-mytweet = "Hi! My name is " + animaldata['name'].my_titleize + ". " + animaldata['desc'].strip_html.slice(0..65) + "... " + animaldata['link']
+mytweet = randomgreeting + " " + animaldata['name'].my_titleize + ". " + animaldata['desc'].strip_html.slice(0..65) + "... " + animaldata['link']
 mytweet.remove_double_whitespace
 
 #post the tweet

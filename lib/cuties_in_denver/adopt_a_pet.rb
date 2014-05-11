7c9e7272328d9a0f8de990997294e1f99bf5f8cd
Peter W <techieshark@gmail.com>
2014-05-11 12:11:12 -0700

    Use Logger to keep track of when we hit server repeatedly.
    Should just send output to console (the `pet` output below the log is from debugging code not included in this commit):
    
    Selene:cuties peter$ rake
    I, [2014-05-11T12:12:13.759008 #16186]  INFO -- : Bummer! The server gave us either no pet or pet w/o pic! I'll try again though.
    I, [2014-05-11T12:12:14.382330 #16186]  INFO -- : Bummer! The server gave us either no pet or pet w/o pic! I'll try again though.
    ...
    pet:
     @attributes=
      {"name"=>"BEAR",
       "pic"=>
        "http://www.petharbor.com/get_image.asp?RES=detail&ID=A3458665&LOCATION=MRCP",
       "id"=>"A3458665",
       "link"=>"http://www.petharbor.com/pet.asp?uaid=MRCP.A3458665",
       "desc"=>"My name is BEAR."}>

--- lib/cuties_in_denver/adopt_a_pet.rb
+++ lib/cuties_in_denver/adopt_a_pet.rb
@@ -1,15 +1,19 @@
 require "net/http"
 require 'json'
 require 'open-uri'
+require 'logger'
 
 class AdoptAPet
   URL = 'http://adopt-a-pet-denver.herokuapp.com/api'
 
   def self.random
+    log = Logger.new(STDOUT)
+    log.level = Logger::INFO #set to Logger:WARN to avoid seing status messages
 
     pet = Pet.new(JSON.parse(Net::HTTP.get_response(URI.parse(URL)).body))
 
     while pet.nil? or pet.pic.nil? # just keep on trying.
+      log.info("Bummer! The server gave us either no pet or pet w/o pic! I'll try again though.")
       pet = Pet.new(JSON.parse(Net::HTTP.get_response(URI.parse(URL)).body))
       #maybe TODO later: exponential backoff rather than continuously hitting the server.
     end

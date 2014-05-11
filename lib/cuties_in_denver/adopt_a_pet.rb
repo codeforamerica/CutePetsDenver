3b926f0098fd623c0e4c823de3ec59dd37c54930
Peter W <techieshark@gmail.com>
2014-05-10 23:54:12 -0700

    Allow API url to be set as an environment variable.
    * Because the URL varies by city, currently.
    * This helps with intercityalization (IC11N, which is a new jargonfile word but that'll be a separate commit!)

--- lib/cuties_in_denver/adopt_a_pet.rb
+++ lib/cuties_in_denver/adopt_a_pet.rb
@@ -3,7 +3,9 @@ require 'json'
 require 'open-uri'
 
 class AdoptAPet
-  URL = 'http://adopt-a-pet-denver.herokuapp.com/api'
+  # Now we need to grab the URL of the city-specific API that tells us about available pets
+  # (If the environment doesn't have a $CUTIES_API_URL variable set, we'll assume we're in Denver.)
+  URL =  ENV.fetch('CUTIES_API_URL', 'http://adopt-a-pet-denver.herokuapp.com/api')
 
   def self.random
       Pet.new(JSON.parse(Net::HTTP.get_response(URI.parse(URL)).body))

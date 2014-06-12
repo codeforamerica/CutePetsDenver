3d1e5c3c22a539b0a45f7625aded9dc04231d46d
beechnut <cloydster@gmail.com>
2014-06-11 21:44:41 -0400

    Make some of the JSON parsing more concise

--- lib/cuties/adopt_a_pet.rb
+++ lib/cuties/adopt_a_pet.rb
@@ -1,4 +1,4 @@
-require "net/http"
+require 'net/http'
 require 'json'
 require 'open-uri'
 require 'pp'
@@ -7,10 +7,11 @@ class AdoptAPet
   URL = 'http://api.petfinder.com/pet.getRandom'
 
   PARAMS = {
-    :format => 'json',
-    :key => ENV.fetch('petfinder_key'),
-    :shelterid => 'MA38', # MSPCA in Jamaica Plain
-    :output => 'full'
+    format:    'json',
+    key:        ENV.fetch('petfinder_key'),
+    shelterid: 'MA38', # MSPCA in Jamaica Plain
+    # id: 29421606,
+    output:    'full'
   }
 
   def self.random
@@ -19,35 +20,31 @@ class AdoptAPet
   end
 
   private
+
+  def self.get_breeds(pet)
+    breed_list = [pet['breeds']['breed']].flatten # Coerces into an Array
+    breed_list = breed_list.map{ |b| b["$t"] }.join(' & ') # Joins into string
+  end
+
+  def self.photo_url(pet)
+    pet['media']['photos']['photo'][2]['$t'] unless pet['media'].nil?
+  end
+
   def self.fetch_pet
     uri = URI(URL)
     uri.query = URI.encode_www_form(PARAMS)
     json = JSON.parse(Net::HTTP.get_response(uri).body)
 
-    PP.pp(json)
+    PP.pp(json)  # Pretty-prints the response in the Terminal
 
-    p = json['petfinder']['pet']
-
-    b = p['breeds']['breed']
-    if b.kind_of?(Array) #if there are multiple breeds put them in a list
-      breeds = []
-      for item in b
-        breeds.push(item['$t'])
-      end
-    else
-      breeds = [b['$t']]
-    end
-
-    if p['media'] != nil
-      picture = p['media']['photos']['photo'][2]['$t']
-    else
-      picture = nil
-    end
+    pet_json  = json['petfinder']['pet']
+    breeds    = get_breeds(pet_json)
+    photo_url = get_photo(pet_json)
 
     pet = {
       "link" => "https://www.petfinder.com/petdetail/" + p['id']['$t'],
       "name" => p['name']['$t'],
-      "pic" => picture,
+      "pic" => photo_url,
       "id" => p['id']['$t'],
       "sex" => p['sex']['$t'],
       "breed" => breeds,

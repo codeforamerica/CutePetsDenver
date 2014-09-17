ea91efe1a6fd7ec5af157d33bc11737b6c71ecf2
Christine Dolendo <chris.dolendo@gmail.com>
2014-09-16 18:16:49 -0700

    integrated city customization into pet query

--- Rakefile
+++ Rakefile
@@ -3,10 +3,11 @@ Dotenv.load
 
 require_relative 'lib/cuties_in_denver'
 
-desc "Retrieve and construct tweet about pet via Petfinder"
+desc "Retrieve and tweet about pet by city via Petfinder"
 task :petfindertweet do 
-  pet = RandomPetRetriever.retrieve_random_pet
+  pet = RandomPetRetriever.retrieve_random_pet('Washington, DC')
   Twit.new(pet).tweet_pet
 end
+
 
 task :default => :twitter
--- lib/cuties_in_denver/random_pet_retriever.rb
+++ lib/cuties_in_denver/random_pet_retriever.rb
@@ -2,9 +2,9 @@ require "petfinder"
 
 class RandomPetRetriever
 
-  def self.retrieve_random_pet
+  def self.retrieve_random_pet(city)
     petfinder = Petfinder::Client.new(ENV["PETFINDER_KEY"], ENV["PETFINDER_SECRET"])
-    pet = petfinder.random_pet
+    pet = petfinder.random_pet(location: city)
     pet_to_tweet = Pet.new(description: pet.description.slice(0..70), photo_url: pet.photos.first.medium, profile_url: "https://www.petfinder.com/petdetail/#{pet.id}")
     pet_to_tweet
   end
